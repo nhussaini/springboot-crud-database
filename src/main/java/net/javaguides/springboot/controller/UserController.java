a9631780da95f7d836074be4431f9aafa17c7f9c
nhussaini <nasrullah_hussaini@yahoo.com>
2023-02-16 20:08:00 -0500

    build get user by id rest api

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -6,10 +6,7 @@ import net.javaguides.springboot.entity.User;
 import net.javaguides.springboot.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -23,6 +20,15 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = userService.createUser(user);
         return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    }
+
+    //build get user by id REST API
+    //http://localhost:8080/api/users/1
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
     }
 
 }
--- src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -13,5 +15,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
     }
 }
